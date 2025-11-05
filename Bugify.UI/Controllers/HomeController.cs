8aef8a228c35cfedd2ee334e7fab0185df09c9d0
Mateusz Filas <116639452+eMeF0@users.noreply.github.com>
2025-11-05 08:26:44 +0100

    Dodaj pliki projektów.

---
+++ Bugify.UI/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Bugify.UI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Bugify.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Bugify.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bugify.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
